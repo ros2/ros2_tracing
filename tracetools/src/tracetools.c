5c4b84520e5b537295779149e1911f4c009ae1d8
Christophe Bedard <bedard.christophe@gmail.com>
2019-05-16 14:16:40 +0200

    Fix coding style

--- tracetools/src/tracetools.c
+++ tracetools/src/tracetools.c
@@ -5,34 +5,39 @@
 #endif
 
 
-bool ros_trace_compile_status() {
+bool ros_trace_compile_status()
+{
 #ifdef WITH_LTTNG
-    return true;
+  return true;
 #else
-    return false;
+  return false;
 #endif
 }
 
-void ros_trace_rcl_init() {
+void ros_trace_rcl_init()
+{
 #ifdef WITH_LTTNG
-    tracepoint(ros2, rcl_init);
+  tracepoint(ros2, rcl_init);
 #endif
 }
 
-void ros_trace_rcl_node_init(const char * node_name, const char * namespace) {
+void ros_trace_rcl_node_init(const char * node_name, const char * namespace)
+{
 #ifdef WITH_LTTNG
-    tracepoint(ros2, rcl_node_init, node_name, namespace);
+  tracepoint(ros2, rcl_node_init, node_name, namespace);
 #endif
 }
 
-void ros_trace_rcl_publisher_init(const char * node_name, const char * namespace) {
+void ros_trace_rcl_publisher_init(const char * node_name, const char * namespace)
+{
 #ifdef WITH_LTTNG
-    tracepoint(ros2, rcl_publisher_init, node_name, namespace);
+  tracepoint(ros2, rcl_publisher_init, node_name, namespace);
 #endif
 }
 
-void ros_trace_rcl_subscription_init(const char * node_name, const char * topic_name) {
+void ros_trace_rcl_subscription_init(const char * node_name, const char * topic_name)
+{
 #ifdef WITH_LTTNG
-    tracepoint(ros2, rcl_subscription_init, node_name, topic_name);
+  tracepoint(ros2, rcl_subscription_init, node_name, topic_name);
 #endif
 }
