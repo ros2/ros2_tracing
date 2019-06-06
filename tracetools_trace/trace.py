77f63f8c95863ec8f0ca4ea18ce664ed547a5bba
Christophe Bedard <fixed-term.christophe.bourquebedard@de.bosch.com>
2019-06-06 09:30:04 +0200

    Expose events through trace entrypoint

--- tracetools_trace/trace.py
+++ tracetools_trace/trace.py
@@ -10,6 +10,10 @@ from tracetools_trace.tools.lttng import (
     lttng_stop,
     lttng_destroy,
 )
+from tracetools_trace.tools.names import (
+    DEFAULT_EVENTS_ROS,
+    DEFAULT_EVENTS_KERNEL,
+)
 
 def main():
     parser = argparse.ArgumentParser(description='Setup and launch an LTTng tracing session.')
@@ -19,13 +23,19 @@ def main():
     parser.add_argument('--path', '-p', dest='path',
                         default='/tmp',
                         help='path of the base directory for trace data (default: %(default)s)')
+    parser.add_argument('--ust', '-u', nargs='*', dest='events_ust', default=DEFAULT_EVENTS_ROS,
+                        help='the ROS UST events to enable (default: all events) [to disable all UST events, provide this flag without any event name]')
+    parser.add_argument('--kernel', '-k', nargs='*', dest='events_kernel', default=DEFAULT_EVENTS_KERNEL,
+                        help='the kernel events to enable (default: all events) [to disable all UST events, provide this flag without any event name]')
     args = parser.parse_args()
 
     session_name = args.session_name
     base_path = args.path
     path = base_path + '/' + session_name
+    ros_events = args.events_ust
+    kernel_events = args.events_kernel
 
-    lttng_setup(session_name, path)
+    lttng_setup(session_name, path, ros_events=ros_events, kernel_events=kernel_events)
     print(f'writting tracing session to: {path}')
 
     input('press enter to start...')
