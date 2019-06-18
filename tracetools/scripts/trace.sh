9cc1f5f29b2e8f587e7c7e08cc6717adec3c1393
Christophe Bedard <fixed-term.christophe.bourquebedard@de.bosch.com>
2019-06-18 09:10:05 +0200

    Add copyright headers

--- tracetools/scripts/trace.sh
+++ tracetools/scripts/trace.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
+# Copyright 2019 Robert Bosch GmbH
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 ## Helper script for ROS tracing
-## Christophe Bedard
-## see: https://github.com/bosch-robotics-cr/tracetools/blob/devel/scripts/example-run-script.sh
+##
 ## Call this by providing these arguments:
 ##  1. a session name [optional; 'ros2' will be used]
 ##  2. a wait time before killing and stopping (in seconds)
