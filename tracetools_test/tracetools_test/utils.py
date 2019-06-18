89dd6415064e2afcfa6a3d1ad8820a64e51f3066
Christophe Bedard <fixed-term.christophe.bourquebedard@de.bosch.com>
2019-06-18 09:10:56 +0200

    Fix python file doc

--- tracetools_test/tracetools_test/utils.py
+++ tracetools_test/tracetools_test/utils.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Utils for tracetools_test
+"""Utils for tracetools_test."""
 
 import shutil
 import time
