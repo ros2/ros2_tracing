5bd910a32223537b3f3370ec01d2851729dfa556
Christophe Bedard <fixed-term.christophe.bourquebedard@de.bosch.com>
2019-07-03 15:21:27 +0200

    Simplify utils

--- tracetools/include/tracetools/utils.hpp
+++ tracetools/include/tracetools/utils.hpp
@@ -25,17 +25,6 @@ const char * _demangle_symbol(const char * mangled);
 const char * _get_symbol_funcptr(void * funcptr);
 
 template<typename T, typename ... U>
-const char * _get_symbol_non_funcptr(std::function<T(U...)> f)
-{
-#if defined(TRACETOOLS_LTTNG_ENABLED) && !defined(_WIN32)
-  return _demangle_symbol(f.target_type().name());
-#else
-  (void)f;
-  return SYMBOL_UNKNOWN;
-#endif
-}
-
-template<typename T, typename ... U>
 const char * get_symbol(std::function<T(U...)> f)
 {
 #if defined(TRACETOOLS_LTTNG_ENABLED) && !defined(_WIN32)
@@ -47,7 +36,7 @@ const char * get_symbol(std::function<T(U...)> f)
     return _get_symbol_funcptr(funcptr);
   }
   // Otherwise we have to go through target_type()
-  return _get_symbol_non_funcptr(f);
+  return _demangle_symbol(f.target_type().name());
 #else
   (void)f;
   return SYMBOL_UNKNOWN;
