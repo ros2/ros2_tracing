2ef48cfe6653b226b82016eac4170afde74e6e13
Christophe Bedard <fixed-term.christophe.bourquebedard@de.bosch.com>
2019-07-31 10:10:33 +0200

    Revert "Replace special character to fix encoding issue"
    This reverts commit c7d609c018acecf4aadb87865a8a014adfa86c20.

--- tracetools_trace/setup.py
+++ tracetools_trace/setup.py
@@ -13,7 +13,7 @@ setup(
     install_requires=['setuptools'],
     maintainer=(
         'Christophe Bedard, '
-        'Ingo Luetkebohle'
+        'Ingo Lütkebohle'
     ),
     maintainer_email=(
         'fixed-term.christophe.bourquebedard@de.bosch.com, '
