c6b7a645e32d74743db9400364d5e53aa559b494
Christophe Bedard <bedard.christophe@gmail.com>
2020-02-28 17:45:57 -0500

    Move TRACETOOLS_LTTNG_ENABLED definition to config.h.in
    Signed-off-by: Christophe Bedard <bedard.christophe@gmail.com>

--- tracetools/src/utils.cpp
+++ tracetools/src/utils.cpp
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include "tracetools/config.h"
+
 #ifdef TRACETOOLS_LTTNG_ENABLED
 #include <dlfcn.h>
 #include <cxxabi.h>
