5902bb334081861672bd68d41c8e0c5f08b9db5c
Christophe Bedard <bedard.christophe@gmail.com>
2020-06-24 11:07:32 -0400

    1.0.2

--- ros2trace/setup.py
+++ ros2trace/setup.py
@@ -5,7 +5,7 @@ package_name = 'ros2trace'
 
 setup(
     name=package_name,
-    version='1.0.1',
+    version='1.0.2',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/' + package_name, ['package.xml']),
--- tracetools_launch/setup.py
+++ tracetools_launch/setup.py
@@ -7,7 +7,7 @@ package_name = 'tracetools_launch'
 
 setup(
     name=package_name,
-    version='1.0.1',
+    version='1.0.2',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/' + package_name, ['package.xml']),
