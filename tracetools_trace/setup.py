e0ad0095ce81279d71deac760a30fce6e8402ee5
Christophe Bedard <bedard.christophe@gmail.com>
2021-04-02 09:54:49 -0400

    Revert "Update Python packages to use generate_setuptools_dict"
    This reverts commit 5fffedf5c4094add249fa07434d1ad2b0a8d8776.
    
    Signed-off-by: Christophe Bedard <bedard.christophe@gmail.com>

--- tracetools_trace/setup.py
+++ tracetools_trace/setup.py
@@ -1,9 +1,11 @@
-from ament_package.generate_setuptools_dict import generate_setuptools_dict
 from setuptools import find_packages
 from setuptools import setup
 
 package_name = 'tracetools_trace'
-package_info = generate_setuptools_dict(
+
+setup(
+    name=package_name,
+    version='2.3.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/' + package_name, ['package.xml']),
@@ -11,12 +13,28 @@ package_info = generate_setuptools_dict(
             ['resource/' + package_name]),
     ],
     install_requires=['setuptools'],
+    maintainer=(
+        'Christophe Bedard, '
+        'Ingo Luetkebohle'
+    ),
+    maintainer_email=(
+        'bedard.christophe@gmail.com, '
+        'ingo.luetkebohle@de.bosch.com'
+    ),
+    author='Christophe Bedard',
+    author_email='fixed-term.christophe.bourquebedard@de.bosch.com',
+    url='https://gitlab.com/ros-tracing/ros2_tracing',
     keywords=[],
+    description='Tools for setting up tracing sessions.',
+    long_description=(
+        'This package provides tools for setting up tracing sessions. '
+        'It also provides basic utilities to provide CLI functionality.'
+    ),
     entry_points={
         'console_scripts': [
             f'trace = {package_name}.trace:main',
         ],
     },
+    license='Apache 2.0',
     tests_require=['pytest'],
 )
-setup(**package_info)
