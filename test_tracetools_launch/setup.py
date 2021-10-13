201968ea33b86fccbb564cff423cff927534ffcd
Christophe Bedard <bedard.christophe@gmail.com>
2021-10-13 19:26:26 -0400

    Move some tests from tracetools_launch to test_tracetools_launch
    Signed-off-by: Christophe Bedard <bedard.christophe@gmail.com>

---
+++ test_tracetools_launch/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages
+from setuptools import setup
+
+package_name = 'test_tracetools_launch'
+
+setup(
+    name=package_name,
+    version='3.1.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/' + package_name, ['package.xml']),
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+    ],
+    install_requires=['setuptools'],
+    maintainer=(
+        'Christophe Bedard, '
+        'Ingo Luetkebohle'
+    ),
+    maintainer_email=(
+        'bedard.christophe@gmail.com, '
+        'ingo.luetkebohle@de.bosch.com'
+    ),
+    author='Christophe Bedard',
+    author_email='fixed-term.christophe.bourquebedard@de.bosch.com',
+    url='https://gitlab.com/ros-tracing/ros2_tracing',
+    keywords=[],
+    description='Tests for the tracetools_launch package.',
+    license='Apache 2.0',
+    tests_require=['pytest'],
+)
--- tracetools_launch/test/tracetools_launch/test_trace_action.py
+++ tracetools_launch/test/tracetools_launch/test_trace_action.py
@@ -13,21 +13,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import io
-import pathlib
-import shutil
-import tempfile
-import textwrap
 from typing import List
 import unittest
 
-from launch import LaunchDescription
-from launch import LaunchService
-from launch.frontend import Parser
-
 from tracetools_launch.action import Trace
 from tracetools_launch.actions.ld_preload import LdPreload
-from tracetools_trace.tools.lttng import is_lttng_installed
 
 
 class TestTraceAction(unittest.TestCase):
@@ -106,96 +96,6 @@ class TestTraceAction(unittest.TestCase):
             self.assertTrue(Trace.has_ust_memory_events(events))
         for events in events_lists_no_match:
             self.assertFalse(Trace.has_ust_memory_events(events))
-
-    def _assert_launch_no_errors(self, actions) -> None:
-        ld = LaunchDescription(actions)
-        ls = LaunchService(debug=True)
-        ls.include_launch_description(ld)
-        assert 0 == ls.run()
-
-    def _assert_launch_frontend_no_errors(self, file) -> Trace:
-        root_entity, parser = Parser.load(file)
-        ld = parser.parse_description(root_entity)
-        ls = LaunchService()
-        ls.include_launch_description(ld)
-        assert 0 == ls.run()
-        trace_action = ld.describe_sub_entities()[0]
-        return trace_action
-
-    def _check_trace_action(self, action, tmpdir) -> None:
-        self.assertEqual('my-session-name', action.session_name)
-        self.assertEqual(tmpdir, action.base_path)
-        self.assertTrue(action.trace_directory.startswith(tmpdir))
-        self.assertEqual([], action.events_kernel)
-        self.assertEqual(['ros2:*', '*'], action.events_ust)
-        self.assertTrue(pathlib.Path(tmpdir).exists())
-
-    @unittest.skipIf(not is_lttng_installed(), 'LTTng is required')
-    def test_action(self) -> None:
-        tmpdir = tempfile.mkdtemp(prefix='TestTraceAction__test_action')
-
-        # Disable kernel events just to not require kernel tracing for the test
-        action = Trace(
-            session_name='my-session-name',
-            base_path=tmpdir,
-            events_kernel=[],
-            events_ust=[
-                'ros2:*',
-                '*',
-            ],
-        )
-        self._assert_launch_no_errors([action])
-        self._check_trace_action(action, tmpdir)
-
-        shutil.rmtree(tmpdir)
-
-    @unittest.skipIf(not is_lttng_installed(), 'LTTng is required')
-    def test_action_frontend_xml(self) -> None:
-        tmpdir = tempfile.mkdtemp(prefix='TestTraceAction__test_frontend_xml')
-
-        xml_file = textwrap.dedent(
-            r"""
-            <launch>
-                <trace
-                    session-name="my-session-name"
-                    base-path="{}"
-                    events-kernel=""
-                    events-ust="ros2:* *"
-                />
-            </launch>
-            """.format(tmpdir)
-        )
-
-        trace_action = None
-        with io.StringIO(xml_file) as f:
-            trace_action = self._assert_launch_frontend_no_errors(f)
-
-        self._check_trace_action(trace_action, tmpdir)
-
-        shutil.rmtree(tmpdir)
-
-    @unittest.skipIf(not is_lttng_installed(), 'LTTng is required')
-    def test_action_frontend_yaml(self) -> None:
-        tmpdir = tempfile.mkdtemp(prefix='TestTraceAction__test_frontend_yaml')
-
-        yaml_file = textwrap.dedent(
-            r"""
-            launch:
-            - trace:
-                session-name: my-session-name
-                base-path: {}
-                events-kernel: ""
-                events-ust: ros2:* *
-            """.format(tmpdir)
-        )
-
-        trace_action = None
-        with io.StringIO(yaml_file) as f:
-            trace_action = self._assert_launch_frontend_no_errors(f)
-
-        self._check_trace_action(trace_action, tmpdir)
-
-        shutil.rmtree(tmpdir)
 
     def test_get_shared_lib_path(self) -> None:
         # Only test not finding a lib for now
