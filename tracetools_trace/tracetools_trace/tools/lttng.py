90c4193784640de80607e6f971af21c2ca2d3cf2
Christophe Bedard <christophe.bedard@apex.ai>
2023-04-11 14:19:16 -0700

    Allow requiring minimum lttng package version for is_lttng_installed
    And require that the lttng Python package be installed.
    
    Signed-off-by: Christophe Bedard <christophe.bedard@apex.ai>

--- tracetools_trace/tracetools_trace/tools/lttng.py
+++ tracetools_trace/tracetools_trace/tools/lttng.py
@@ -20,25 +20,36 @@ import subprocess
 import sys
 from typing import Optional
 
+from packaging.version import Version
+
 try:
     from . import lttng_impl
-
     _lttng = lttng_impl  # type: ignore
-
-    # Check lttng module version
-    from packaging.version import Version
-    current_version = _lttng.get_version()
-    LTTNG_MIN_VERSION = '2.10.7'
-    if current_version is None or current_version < Version(LTTNG_MIN_VERSION):
-        print(
-            f'lttng module version >={LTTNG_MIN_VERSION} required, found {str(current_version)}',
-            file=sys.stderr,
-        )
 except ImportError:
     # Fall back on stub functions so that this still passes linter checks
     from . import lttng_stub
-
     _lttng = lttng_stub  # type: ignore
+
+
+def get_lttng_version() -> Optional[Version]:
+    """
+    Get version of lttng Python package.
+
+    :return: the version of the lttng Python package, or `None` if it is not available
+    """
+    if not hasattr(_lttng, 'get_version') or not callable(_lttng.get_version):
+        return None
+    return _lttng.get_version()
+
+
+# Check lttng module version
+current_version = get_lttng_version()
+LTTNG_MIN_VERSION = '2.10.7'
+if current_version is None or current_version < Version(LTTNG_MIN_VERSION):
+    print(
+        f'lttng module version >={LTTNG_MIN_VERSION} required, found {str(current_version)}',
+        file=sys.stderr,
+    )
 
 
 def lttng_init(**kwargs) -> Optional[str]:
@@ -69,15 +80,24 @@ def lttng_fini(**kwargs) -> None:
     _lttng.destroy(**kwargs)
 
 
-def is_lttng_installed() -> bool:
+def is_lttng_installed(
+    *,
+    minimum_version: Optional[str] = None,
+) -> bool:
     """
     Check if LTTng is installed.
 
     It first checks if the OS can support LTTng.
-    If so, it then simply checks if LTTng is installed using the 'lttng' command.
+    If so, it then simply checks if LTTng is installed using the 'lttng' command, and checks if the
+    lttng Python package is installed (python3-lttng).
 
-    :return: True if it is installed, False otherwise
+    Optionally, a minimum version can also be specified for the lttng Python package.
+
+    :param minimum_version: the minimum required lttng Python package version
+    :return: True if LTTng and the lttng Python package are installed, and optionally if the
+        version of lttng Python package is sufficient, False otherwise
     """
+    # Check system
     message_doc = (
         'Cannot trace. See documentation at: '
         'https://github.com/ros2/ros2_tracing'
@@ -86,6 +106,7 @@ def is_lttng_installed() -> bool:
     if 'Linux' != system:
         print(f"System '{system}' does not support LTTng.\n{message_doc}", file=sys.stderr)
         return False
+    # Check if LTTng (CLI) is installed
     try:
         process = subprocess.Popen(
             ['lttng', '--version'],
@@ -95,7 +116,25 @@ def is_lttng_installed() -> bool:
         _, stderr = process.communicate()
         if 0 != process.returncode:
             raise RuntimeError(stderr.decode())
-        return True
     except (RuntimeError, FileNotFoundError) as e:
         print(f'LTTng not found: {e}\n{message_doc}', file=sys.stderr)
         return False
+    # Check if lttng Python package is installed
+    lttng_version = get_lttng_version()
+    if not lttng_version:
+        print(
+            f'lttng Python package (python3-lttng) not installed\n{message_doc}',
+            file=sys.stderr,
+        )
+        return False
+    # Check if lttng Python package version is sufficient
+    if minimum_version and lttng_version < Version(minimum_version):
+        print(
+            (
+                f'lttng Python package (python3-lttng) version >={minimum_version} required, '
+                f'found {str(lttng_version)}'
+            ),
+            file=sys.stderr,
+        )
+        return False
+    return True
