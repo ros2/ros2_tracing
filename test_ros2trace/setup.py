045e8925b744bb978963f8a332b432c94e84d8c1
Christophe Bedard <christophe.bedard@apex.ai>
2023-04-18 14:52:38 -0700

    Move ros2trace tests to new test_ros2trace package (#63)
    Signed-off-by: Christophe Bedard <christophe.bedard@apex.ai>

---
+++ test_ros2trace/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages
+from setuptools import setup
+
+package_name = 'test_ros2trace'
+
+setup(
+    name=package_name,
+    version='6.2.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/' + package_name, ['package.xml']),
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Christophe Bedard',
+    maintainer_email='bedard.christophe@gmail.com',
+    author='Christophe Bedard',
+    author_email='bedard.christophe@gmail.com',
+    url='https://github.com/ros2/ros2_tracing',
+    keywords=[],
+    description='Tests for the ros2trace package.',
+    license='Apache 2.0',
+    tests_require=['pytest'],
+)
