ebac435a0bc24e5ebd31fe0eda3f1e6d96264482
h-suzuki-isp <h-suzuki@isp.co.jp>
2024-03-22 13:57:54 +0900

    Fix: copyright
    Signed-off-by: h-suzuki-isp <h-suzuki@isp.co.jp>

--- test_tracetools/src/test_generic_ping.cpp
+++ test_tracetools/src/test_generic_ping.cpp
@@ -1,4 +1,5 @@
-// Copyright 2024 Research Institute of Systems Planning, Inc.
+// Copyright 2019 Robert Bosch GmbH
+// Copyright 2024 TIER IV, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
