f1e79ebba8b8d435c97e093402d5b1645f3aae4b
Christophe Bedard <christophe.bedard@apex.ai>
2024-11-11 13:40:32 -0800

    Refactor and split test_service into test_{service,client} (#144)
    Signed-off-by: Christophe Bedard <christophe.bedard@apex.ai>

--- test_tracetools/src/test_service_ping.cpp
+++ test_tracetools/src/test_service_ping.cpp
@@ -19,71 +19,33 @@
 #include "std_srvs/srv/empty.hpp"
 #include "test_tracetools/mark_process.hpp"
 
-using namespace std::chrono_literals;
-
-#define NODE_NAME "test_service_ping"
-#define SERVICE_NAME "pong"
-#define CLIENT_NAME "ping"
-
-class PingNode : public rclcpp::Node
-{
-public:
-  explicit PingNode(rclcpp::NodeOptions options)
-  : Node(NODE_NAME, options)
-  {
-    srv_ = this->create_service<std_srvs::srv::Empty>(
-      SERVICE_NAME,
-      std::bind(
-        &PingNode::service_callback,
-        this,
-        std::placeholders::_1,
-        std::placeholders::_2,
-        std::placeholders::_3));
-    client_ = this->create_client<std_srvs::srv::Empty>(
-      CLIENT_NAME);
-    timer_ = this->create_wall_timer(
-      500ms,
-      std::bind(&PingNode::timer_callback, this));
-  }
-
-private:
-  void service_callback(
-    const std::shared_ptr<rmw_request_id_t> request_header,
-    const std::shared_ptr<std_srvs::srv::Empty::Request> request,
-    const std::shared_ptr<std_srvs::srv::Empty::Response> response)
-  {
-    (void)request_header;
-    (void)request;
-    (void)response;
-    RCLCPP_INFO(this->get_logger(), "got request");
-    rclcpp::shutdown();
-  }
-
-  void timer_callback()
-  {
-    auto req = std::make_shared<std_srvs::srv::Empty::Request>();
-    client_->async_send_request(req);
-  }
-
-  rclcpp::Service<std_srvs::srv::Empty>::SharedPtr srv_;
-  rclcpp::Client<std_srvs::srv::Empty>::SharedPtr client_;
-  rclcpp::TimerBase::SharedPtr timer_;
-};
-
 int main(int argc, char * argv[])
 {
   test_tracetools::mark_trace_test_process();
 
   rclcpp::init(argc, argv);
 
-  rclcpp::executors::SingleThreadedExecutor exec;
-  auto ping_node = std::make_shared<PingNode>(rclcpp::NodeOptions());
-  exec.add_node(ping_node);
+  auto node = rclcpp::Node::make_shared("test_service_ping");
 
-  printf("spinning\n");
-  exec.spin();
+  auto client = node->create_client<std_srvs::srv::Empty>("ping");
+  RCLCPP_INFO(node->get_logger(), "waiting for service");
+  while (!client->wait_for_service(std::chrono::seconds(10))) {
+    if (!rclcpp::ok()) {
+      return 1;
+    }
+  }
 
-  // Will actually be called inside the node's service callback
+  auto request = std::make_shared<std_srvs::srv::Empty::Request>();
+  auto result_future = client->async_send_request(request);
+  RCLCPP_INFO(node->get_logger(), "sent request");
+  if (rclcpp::spin_until_future_complete(node, result_future) !=
+    rclcpp::FutureReturnCode::SUCCESS)
+  {
+    client->remove_pending_request(result_future);
+    return 1;
+  }
+  auto result = result_future.get();
+  RCLCPP_INFO(node->get_logger(), "got response");
   rclcpp::shutdown();
   return 0;
 }
