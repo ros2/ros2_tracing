32eedd0ce2132f169a914f5d9b166f7ef16f4f1b
Shravan Deva <devashravan7@gmail.com>
2025-05-28 20:19:34 +0530

    Replace ros_trace_enabled_* call with _FUNC_TRACEPOINT_ENABLED macro
    Signed-off-by: Shravan Deva <devashravan7@gmail.com>

--- tracetools/include/tracetools/tracetools.h
+++ tracetools/include/tracetools/tracetools.h
@@ -109,7 +109,7 @@
  * This is the preferred method over calling the underlying function directly.
  */
 #  define TRACETOOLS_TRACEPOINT_ENABLED(event_name) \
-  ros_trace_enabled_ ## event_name()
+  _FUNC_TRACEPOINT_ENABLED(event_name)()
 /// Call a tracepoint, without checking if it is enabled.
 /**
  * Combine this with `TRACEPOINT_ENABLED()` to check if a tracepoint is enabled before triggering
