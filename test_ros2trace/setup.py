9e6e83bb26d3427e8b1c9c53856a46bed26b6c31
mosfet80 <10235105+mosfet80@users.noreply.github.com>
2025-07-10 18:24:26 +0200

    fix setuptools deprecation (#189)
    Signed-off-by: mosfet80 <10235105+mosfet80@users.noreply.github.com>
    Co-authored-by: Christophe Bedard <bedard.christophe@gmail.com>
    (cherry picked from commit 526967c1a0ad3208fe6c28e0cf16f2b045ed5241)
    
    # Conflicts:
    #	test_ros2trace/setup.py

--- test_ros2trace/setup.py
+++ test_ros2trace/setup.py
@@ -1,11 +1,11 @@
 from setuptools import find_packages
 from setuptools import setup
 
-package_name = 'test_tracetools_launch'
+package_name = 'test_ros2trace'
 
 setup(
     name=package_name,
-    version='4.1.1',
+    version='8.8.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/' + package_name, ['package.xml']),
@@ -13,19 +13,14 @@ setup(
             ['resource/' + package_name]),
     ],
     install_requires=['setuptools'],
-    maintainer=(
-        'Christophe Bedard, '
-        'Ingo Luetkebohle'
-    ),
-    maintainer_email=(
-        'bedard.christophe@gmail.com, '
-        'ingo.luetkebohle@de.bosch.com'
-    ),
+    zip_safe=True,
+    maintainer='Christophe Bedard',
+    maintainer_email='bedard.christophe@gmail.com',
     author='Christophe Bedard',
-    author_email='fixed-term.christophe.bourquebedard@de.bosch.com',
-    url='https://gitlab.com/ros-tracing/ros2_tracing',
+    author_email='bedard.christophe@gmail.com',
+    url='https://github.com/ros2/ros2_tracing',
     keywords=[],
-    description='Tests for the tracetools_launch package.',
+    description='Tests for the ros2trace package.',
     license='Apache 2.0',
     extras_require={
         'test': [
