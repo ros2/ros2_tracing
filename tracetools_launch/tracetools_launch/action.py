377cd5df28af4463dbe6ebad1f37579290c14367
Shravan Deva <devashravan7@gmail.com>
2025-08-13 04:22:11 +0530

    Update log statements of trace action
    Signed-off-by: Shravan Deva <devashravan7@gmail.com>

--- tracetools_launch/tracetools_launch/action.py
+++ tracetools_launch/tracetools_launch/action.py
@@ -468,7 +468,8 @@ class Trace(Action):
                 )
                 if self._trace_directory is None:
                     return False
-                self._logger.info(f'Writing tracing session to: {self._trace_directory}')
+                if not is_snapshot_session:
+                    self._logger.info(f'Writing tracing session to: {self._trace_directory}')
                 self._logger.debug(f'UST events: {events_ust}')
                 self._logger.debug(f'Kernel events: {events_kernel}')
                 self._logger.debug(f'Syscalls: {syscalls}')
