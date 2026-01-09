1e930d6761abfc88a1498f0dbeb010845162bdfc
Raphael van Kempen <raphael.vankempen@ika.rwth-aachen.de>
2026-01-09 18:40:17 +0100

    fix code style
    Signed-off-by: Raphael van Kempen <raphael.vankempen@ika.rwth-aachen.de>

--- tracetools/include/tracetools/tracetools.h
+++ tracetools/include/tracetools/tracetools.h
@@ -670,7 +670,7 @@ _DECLARE_TRACEPOINT(
 /// `message_link_partial_sync`
 /**
  * Message link annotation for 2-to-M link.
- * 
+ *
  * \param[in] subs pointers to the `rcl` handles of the subscriptions in the link
  * \param[in] num_subs the number of subscriptions in the link
  * \param[in] pubs pointers to the `rcl` handles of the publishers in the link
