6582b371292a34b1b12e4d16b4c28835492a4054
Reece Holland <reece.j.holland@gmail.com>
2026-01-22 06:20:22 +0800

    lttngpy: adjust include ordering in event.cpp
    Signed-off-by: Reece Holland <reece.j.holland@gmail.com>

--- lttngpy/src/lttngpy/event.cpp
+++ lttngpy/src/lttngpy/event.cpp
@@ -12,9 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <string.h>
-
 #include <lttng/lttng.h>
+#include <string.h>
 
 #include <map>
 #include <optional>
