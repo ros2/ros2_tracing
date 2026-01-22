e39aed20a5df1b46263a83cf7348b69f0b689860
RHolland <17493785+reeceholland@users.noreply.github.com>
2026-01-23 06:26:59 +0800

    Use <lttng/lttng.h> in lttngpy and clean up includes (#222)
    Signed-off-by: Reece Holland <reece.j.holland@gmail.com>
    (cherry picked from commit 91ff6165773e1177a5fd41431d1b1dc274ac4ee1)
    
    # Conflicts:
    #	lttngpy/src/lttngpy/snapshot.cpp

--- lttngpy/src/lttngpy/session.cpp
+++ lttngpy/src/lttngpy/session.cpp
@@ -12,8 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <lttng/lttng-error.h>
-#include <lttng/session.h>
+#include <lttng/lttng.h>
 
 #include <set>
 #include <string>
