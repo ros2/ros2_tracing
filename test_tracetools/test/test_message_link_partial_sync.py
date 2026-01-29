07b24ac210c0d359d9bb22674781f42b848d2d96
Raphael van Kempen <raphael.vankempen@ika.rwth-aachen.de>
2026-01-29 08:15:29 +0000

    fix partial sync test

--- test_tracetools/test/test_message_link_partial_sync.py
+++ test_tracetools/test/test_message_link_partial_sync.py
@@ -38,10 +38,11 @@ class TestPubSub(TraceTestCase):
         # Check events as set
         self.assertEventsSet(self._events_ros)
 
-        # The test_message_link_partial_sync node subscribes to /ping and /the_topic,
-        # and publishes to /pong. It annotates this relationship with message_link_partial_sync.
+        # The test_message_link_partial_sync node subscribes to /ping and /the_topic and publishes
+        # to /pong. While the relationship netween /the_topic and /pong can be inferred
+        # directly, the link to /ping is annotated with message_link_partial_sync.
 
-        # Get subscription handles of input topics and publisher handle of output topic
+        # Get subscription handles of /ping topic and publisher handle of /pong topic
         rcl_subscription_init_events = self.get_events_with_name(tp.rcl_subscription_init)
         ping_rcl_subscription_init_event = self.get_event_with_field_value_and_assert(
             'topic_name',
@@ -50,16 +51,6 @@ class TestPubSub(TraceTestCase):
             allow_multiple=False,
         )
         ping_sub_handle = self.get_field(ping_rcl_subscription_init_event, 'subscription_handle')
-        the_topic_rcl_subscription_init_event = self.get_event_with_field_value_and_assert(
-            'topic_name',
-            '/the_topic',
-            rcl_subscription_init_events,
-            allow_multiple=False,
-        )
-        the_topic_sub_handle = self.get_field(
-            the_topic_rcl_subscription_init_event,
-            'subscription_handle',
-        )
         publisher_init_events = self.get_events_with_name(tp.rcl_publisher_init)
         message_link_partial_sync_publisher_init_event = self.get_event_with_field_value_and_assert(
             'topic_name',
@@ -79,20 +70,15 @@ class TestPubSub(TraceTestCase):
         message_link_event = message_link_events[0]
 
         # Verify the event contains the correct subscription and publisher handles
-        # The event should have both subscriptions linked to the pong publisher
+        # The event should have the /ping subscription linked to the /pong publisher
         link_subs = self.get_field(message_link_event, 'subscription_handles')
         link_pubs = self.get_field(message_link_event, 'publisher_handles')
 
-        self.assertEqual(len(link_subs), 2, 'Expected 2 subscription handles')
+        self.assertEqual(len(link_subs), 1, 'Expected 1 subscription handle')
         self.assertIn(
             ping_sub_handle,
             link_subs,
             'Expected /ping subscription handle in message link',
-        )
-        self.assertIn(
-            the_topic_sub_handle,
-            link_subs,
-            'Expected /the_topic subscription handle in message link',
         )
         self.assertEqual(len(link_pubs), 1, 'Expected 1 publisher handle')
         self.assertIn(
