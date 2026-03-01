4eeebdbd8a3be866ccb97ddcbd326de45a0eaa85
Michael Carlstrom <rmc@carlstrom.com>
2026-03-01 11:50:40 -0800

    Ignore A0005 (#237)
    Signed-off-by: Michael Carlstrom <rmc@carlstrom.com>

--- tracetools_trace/tracetools_trace/trace.py
+++ tracetools_trace/tracetools_trace/trace.py
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# flake8: noqa: A005
+
 """Entrypoint/script to setup and start an LTTng tracing session."""
 
 import argparse
