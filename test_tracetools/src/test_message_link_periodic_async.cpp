3441783df24b63a5554a140f92fc4afaf26193d3
Raphael van Kempen <raphael.vankempen@ika.rwth-aachen.de>
2026-03-02 13:34:56 +0100

    update copyright notice for new files
    Signed-off-by: Raphael van Kempen <raphael.vankempen@ika.rwth-aachen.de>

--- test_tracetools/src/test_message_link_periodic_async.cpp
+++ test_tracetools/src/test_message_link_periodic_async.cpp
@@ -1,4 +1,4 @@
-// Copyright 2019 Robert Bosch GmbH
+// Copyright 2026 Raphael van Kempen
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
